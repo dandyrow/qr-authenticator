38cb45950e19ecc6b96615a8650eb4a256b31df1
dandyrow <daniel@daniellowry.co.uk>
2022-02-13 15:40:44 +0000

    Improved permission handling
    If the user denies camera permission it will now alert them saying the
    app needs it to function.
    
    TODO: start scan after coming back from changing permission settings

--- src/pages/ScanPage.vue
+++ src/pages/ScanPage.vue
@@ -5,20 +5,46 @@
     :scan-active="qrScanner.scanActive"
   >
     <scanBox />
+    <ion-alert
+      :is-open="!permission"
+      header="Need Permission"
+      message="This app needs permission to access your camera to scan QR codes."
+      :buttons="alertButtons"
+    />
   </BaseLayout>
 </template>
 
 <script setup lang="ts">
-import { onIonViewDidEnter, onIonViewWillLeave } from '@ionic/vue';
+import {ref} from 'vue';
+import { onIonViewDidEnter, onIonViewWillLeave, IonAlert } from '@ionic/vue';
 
 import scanBox from '@/components/scanBox.vue';
 import QRScanner from '@/services/qrScanner';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 
 const qrScanner = new QRScanner();
+const permission = ref(true);
+const alertButtons = [{
+            text: 'Cancel',
+            role: 'cancel',
+          },
+          {
+            text: 'Open Settings',
+            handler: () => {
+              BarcodeScanner.openAppSettings();
+            },
+          }];
+
+async function scan() {
+  if (permission.value) {
+    const result = await qrScanner.startScan();
+    console.log(result);
+  }
+}
 
 onIonViewDidEnter(async () => {
-  const result = await qrScanner.startScan();
-  console.log(result);
+  permission.value = await qrScanner.checkPermissions();
+  scan();
 });
 
 //Stop scanning when leave to a different tab
