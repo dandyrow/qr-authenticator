4a9ff70cc982e2fee2fd2bbb6f5c8ef83bca9620
dandyrow <daniel@daniellowry.co.uk>
2022-03-18 01:40:40 +0000

    Added API funstion to post auth to server
    Work in progress. Need to properly integrate with user authentication.

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -1,15 +1,39 @@
-const authUrl = 'http://localhost:8080/login';
+import { useAuth } from '@/stores/auth.store';
+
+const authUrl = 'http://localhost:8080/user/login';
 
 interface Tokens {
-  accessToken: string,
-  refreshToken: string,
+  accessToken: string;
+  refreshToken: string;
 }
 
-export async function fetchTokens(username: string, password: string): Promise<Tokens> {
-  const res = await fetch('authUrl', {
+export async function fetchTokens(
+  username: string,
+  password: string,
+): Promise<Response> {
+  console.log(`Username: ${username}, Password: ${password}`);
+  const res = await fetch(authUrl, {
     method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ username, password }),
   });
 
-  return res.json();
+  return res;
+}
+
+function _getTokenHeader(): string {
+  const store = useAuth();
+  return store.accessToken ?? '';
+}
+
+export async function postAuthSuccess(authPayload: any) {
+  const res = await fetch('http://localhost:8080/auth', {
+    method: 'POST',
+    // headers: {
+    //   'Authorization': `Bearer ${_getTokenHeader()}`,
+    // },
+    body: JSON.stringify(authPayload),
+  });
+
+  return res;
 }
