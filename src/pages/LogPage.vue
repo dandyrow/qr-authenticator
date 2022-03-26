5d9fc46670c01681b45f968c1b2d825214cbb7d6
dandyrow <daniel@daniellowry.co.uk>
2022-03-26 00:14:01 +0000

    Added feature comming soon card on log page

--- src/pages/LogPage.vue
+++ src/pages/LogPage.vue
@@ -1,11 +1,22 @@
 <template>
   <base-layout page-title="Authentication Log">
-    <IonList>
-      <IonItem />
-    </IonList>
+    <IonCard>
+      <IonCardHeader>
+        <IonCardTitle> Feature coming soon! </IonCardTitle>
+      </IonCardHeader>
+      <IonCardContent>
+        Authentication history log has not yet been implemented but is a planned
+        feature for a future release.
+      </IonCardContent>
+    </IonCard>
   </base-layout>
 </template>
 
 <script setup lang="ts">
-import { IonList, IonItem } from '@ionic/vue';
+import {
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+} from '@ionic/vue';
 </script>
